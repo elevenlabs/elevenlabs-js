13bc9cc59d8d58603636db541c6b27b1a37cfbfd
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-19 02:57:29 +0000

    Release 0.0.6

--- src/api/resources/models/client/Client.ts
+++ src/api/resources/models/client/Client.ts
@@ -47,7 +47,7 @@ export class Models {
                         : undefined,
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "",
-                "X-Fern-SDK-Version": "0.0.5",
+                "X-Fern-SDK-Version": "0.0.6",
             },
             contentType: "application/json",
             timeoutMs: requestOptions?.timeoutInSeconds != null ? requestOptions.timeoutInSeconds * 1000 : 60000,
