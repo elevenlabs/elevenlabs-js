669d545d85786000c410702c1a4b4984260adbb6
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-05 21:49:10 +0000

    SDK regeneration

--- src/api/resources/textToSoundEffects/client/Client.ts
+++ src/api/resources/textToSoundEffects/client/Client.ts
@@ -52,7 +52,7 @@ export class TextToSoundEffects {
                         : undefined,
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "elevenlabs",
-                "X-Fern-SDK-Version": "0.13.0",
+                "X-Fern-SDK-Version": "0.14.0",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
