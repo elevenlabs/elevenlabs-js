df11aec517aa3bb6fd2168add434c83eb9155e4f
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-07-09 21:10:11 -0400

    improve getBadResponseBody.ts

--- src/core/fetcher/getBadResponseBody.ts
+++ src/core/fetcher/getBadResponseBody.ts
@@ -2,22 +2,35 @@ import { fromJson } from "../json";
 import { getResponseBody } from "./getResponseBody";
 
 export async function getBadResponseBody(response: Response): Promise<unknown> {
-    const contentType = response.headers.get("Content-Type");
-    if (contentType == null) {
+    let contentType = response.headers.get("Content-Type")?.toLowerCase();
+    if (contentType == null || contentType.length === 0) {
         return getResponseBody(response);
     }
 
-    const responseType = contentType.split(";")[0].trim();
-    switch (responseType) {
+    if (contentType.indexOf(";") !== -1) {
+        contentType = contentType.split(";")[0].trim();
+    }
+    switch (contentType) {
+        case "application/hal+json":
         case "application/json":
+        case "application/ld+json":
+        case "application/problem+json":
         case "application/vnd.api+json":
         case "text/json":
             const text = await response.text();
             return text.length > 0 ? fromJson(text) : undefined;
-        case "text/plain":
+        case "application/problem+xml":
+        case "application/xml":
         case "text/html":
+        case "text/plain":
+        case "text/xml":
             return await response.text();
         default:
+            if (contentType.startsWith("application/vnd.") && contentType.endsWith("+json")) {
+                const text = await response.text();
+                return text.length > 0 ? fromJson(text) : undefined;
+            }
+
             return response.body;
     }
 }
