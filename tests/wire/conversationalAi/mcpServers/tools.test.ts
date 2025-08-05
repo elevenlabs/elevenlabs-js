a6883d9edce70efb9fe5916756d3a59228796f5e
Angelo Giacco <giacco.angelo.s@gmail.com>
2025-08-06 01:59:19 +0200

    fix: update tool test

--- tests/wire/conversationalAi/mcpServers/tools.test.ts
+++ tests/wire/conversationalAi/mcpServers/tools.test.ts
@@ -26,7 +26,7 @@ describe("Tools", () => {
                         type: "object",
                     },
                     outputSchema: { key: "value" },
-                    _meta: { key: "value" },
+                    meta: { key: "value" },
                 },
                 {
                     name: "tool2",
@@ -34,7 +34,7 @@ describe("Tools", () => {
                     description: "Description of tool2",
                     inputSchema: { properties: {}, type: "object" },
                     outputSchema: { key: "value" },
-                    _meta: { key: "value" },
+                    meta: { key: "value" },
                 },
             ],
             error_message: "error_message",
@@ -72,7 +72,7 @@ describe("Tools", () => {
                     outputSchema: {
                         key: "value",
                     },
-                    _meta: {
+                    meta: {
                         key: "value",
                     },
                 },
@@ -87,7 +87,7 @@ describe("Tools", () => {
                     outputSchema: {
                         key: "value",
                     },
-                    _meta: {
+                    meta: {
                         key: "value",
                     },
                 },
