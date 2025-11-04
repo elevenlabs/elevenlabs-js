5bcb2571767130076924a130409a2c47b62aee9f
Paul Asjes <paul.asjes@elevenlabs.io>
2025-11-04 14:22:46 +0100

    sort exports

--- src/wrapper/realtime/index.ts
+++ src/wrapper/realtime/index.ts
@@ -1,3 +1,21 @@
-export { RealtimeConnection, RealtimeEvents, type Config, type InputAudioChunk, type WordsItem, type WordsItemType, type FinalTranscriptMessage, type FinalTranscriptWithTimestampsMessage, type PartialTranscriptMessage, type SessionStartedMessage } from "./connection";
-export { ScribeRealtime, AudioFormat, CommitStrategy, type AudioOptions, type UrlOptions } from "./scribe";
+export {
+    RealtimeConnection,
+    RealtimeEvents,
+    type Config,
+    type InputAudioChunk,
+    type WordsItem,
+    type WordsItemType,
+    type FinalTranscriptMessage,
+    type FinalTranscriptWithTimestampsMessage,
+    type PartialTranscriptMessage,
+    type SessionStartedMessage
+} from "./connection";
+
+export {
+    ScribeRealtime,
+    AudioFormat,
+    CommitStrategy,
+    type AudioOptions,
+    type UrlOptions
+} from "./scribe";
 
